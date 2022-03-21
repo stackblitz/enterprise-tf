341031c04fd079d67d8fbe7c91939f16687558e4
Adam Clarke <adamclarkewastaken@gmail.com>
2022-03-21 16:04:57 -0400

    add additional subnets for contrib/aws-private-vpc

--- contrib/aws-private-vpc/main.tf
+++ contrib/aws-private-vpc/main.tf
@@ -12,11 +12,13 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs            = ["${local.region}a"]
-  intra_subnets  = ["10.0.1.0/24"]
-  public_subnets = ["10.0.101.0/28"]
+  intra_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets = ["10.0.101.0/28", "10.0.102.0/28"]
 
-  enable_nat_gateway = false
-  enable_vpn_gateway = true
+  enable_nat_gateway   = false
+  enable_vpn_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Terraform   = "true"
